a0fba8eddb71e0a47efbea09ee988d2608aa32b8
Saee Patil <152836158+SAEEMPATIL@users.noreply.github.com>
2025-04-21 14:48:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Route setup
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => app.listen(5000, () => console.log('Server is running on port 5000')))
+    .catch(err => console.log(err));
 
