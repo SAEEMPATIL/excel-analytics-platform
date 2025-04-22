34dc3d23ab79832ac1eb0a4f80558eccf76403e0
Ayna Gopakumar <aynag11@gmail.com>
2025-04-23 04:06:37 +0530

    Add auth controllers, routes, User model; update server setup

--- server/server.js
+++ server/server.js
@@ -8,10 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // Route setup
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => app.listen(5000, () => console.log('Server is running on port 5000')))
-    .catch(err => console.log(err));
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('MongoDB error', err));
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  
